eac252bb9ad06692d777808bf9aae701a40c1327
lantos14 <lantos891213@gmail.com>
2018-05-10 12:09:37 +0200

    printed my favorite number as instructed

---
+++ week-01/day-4/favorite-number.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let favoriteNumber: number = 8
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+let myFavoriteNumber: number = 13
+
+console.log (`My favorite number is: ${myFavoriteNumber}`);
