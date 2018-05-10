aeb6267f32b7e5de24bfa7d543bbf8f1c8fd0418
lantos14 <lantos891213@gmail.com>
2018-05-10 12:33:49 +0200

    printed out the variables as instructed

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+
+// Your name as a string
+
+let myName: string = 'Tibor Lantos';
+
+// Your age as an integer
+
+let myAge: number = 28;
+
+// Your height in meters as a float
+
+let myHeightInMeters: number = 1.8;
+
+// Whether you are married or not as a boolean
+
+let maritalStatus: boolean = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeightInMeters);
+console.log(maritalStatus);
