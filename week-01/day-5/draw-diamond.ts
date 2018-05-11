a7f774c40c940a085d96f170daacd9e7d3639b90
lantos14 <lantos891213@gmail.com>
2018-05-11 11:27:49 +0200

    modified indentation based on style guide

--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -21,23 +21,23 @@ let lineCount: number = 7;
 for (let i: number = 0; i <= lineCount/2-1; i++){
   let output: string = '';
 
-      // space
+    // space
 
-      for (let j: number = 0; j < lineCount/2-i; j++){
-          output = output + ' ';
-      }
+    for (let j: number = 0; j < lineCount/2-i; j++){
+      output = output + ' ';
+    }
 
-      // star
-      if (lineCount % 2 === 1) {
-        for (let k: number = 1; k <= (i*2+1); k++){
-          output = output + '*';
-        } 
+    // star
+    if (lineCount % 2 === 1) {
+      for (let k: number = 1; k <= (i*2+1); k++){
+        output = output + '*';
+      } 
+    }
+    if (lineCount % 2 === 0) {
+      for (let k: number = 0; k <= (i*2); k++){
+        output = output + '*';
       }
-      if (lineCount % 2 === 0) {
-        for (let k: number = 0; k <= (i*2); k++){
-          output = output + '*';
-        }
-      }
+    }
 
   console.log (output);
 }
