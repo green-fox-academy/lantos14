4b3517b17b92ded68409ba2ed4438f21352338f8
lantos14 <lantos891213@gmail.com>
2018-05-14 14:25:56 +0200

    function prints out the elements with added 'a'

---
+++ week-02/day-1/append-a.ts
@@ -0,0 +1,13 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+const animals = ['kuty', 'macsk', 'cic'];
+
+animals.forEach (function (item: string) {
+  item = `${item}a`;
+  console.log (item)
+})
+
+
+
