1bb69008d1df897fd65d4056a51d478bda8a6bce
lantos14 <lantos891213@gmail.com>
2018-05-14 12:25:24 +0200

    code prints it out, whether secondList has more elements or no -- with boolean value

---
+++ week-02/day-1/compare-length.ts
@@ -0,0 +1,11 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+const firstList = [1, 2, 3];
+const secondList = [4, 5];
+
+console.log(secondList.length > firstList.length)
+
