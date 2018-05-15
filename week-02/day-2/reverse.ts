af7983c60a0d5d0c6914f62e37e2346af18a1995
lantos14 <lantos891213@gmail.com>
2018-05-15 11:59:07 +0200

    function reverse the input str and returns it

---
+++ week-02/day-2/reverse.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+let tempStr = '';
+
+function reverse (input) {
+  for (let i:number = 1; i <= input.length; i++) {
+    tempStr = tempStr + input[input.length-i];
+  }
+  return tempStr;
+}
+
+
+console.log(reverse(reversed));
+export = reverse;
