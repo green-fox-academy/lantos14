4b23bbb37584e9c764ca1b1e3b899d7d09fd50bb
lantos14 <lantos891213@gmail.com>
2018-05-23 17:41:42 +0200

    added export

--- week-03/day-2/blogSpot.ts
+++ week-03/day-2/blogSpot.ts
@@ -13,9 +13,9 @@ export class blogSpot {
 }
 
 
-let blogNote01 = new blogSpot ('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet', '2000.05.04')
-let blogNote02 = new blogSpot ('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything', '2010.10.10')
-let blogNote03 = new blogSpot ('One Engineer Is Trying to Get IBM to Reckon With Trump', 'William Turton', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing', '2017.03.28.')
+export let blogNote01 = new blogSpot ('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet', '2000.05.04')
+export let blogNote02 = new blogSpot ('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything', '2010.10.10')
+export let blogNote03 = new blogSpot ('One Engineer Is Trying to Get IBM to Reckon With Trump', 'William Turton', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing', '2017.03.28.')
 
 console.log (blogNote01);
 
