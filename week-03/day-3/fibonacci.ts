b831474cba627460d8ec4c03c60e67dad2e4fb66
lantos14 <lantos891213@gmail.com>
2018-05-24 12:32:36 +0200

    function returns the nth fibonacci number starting from 0

---
+++ week-03/day-3/fibonacci.ts
@@ -0,0 +1,19 @@
+// The fibonacci sequence is a famous bit of mathematics, and it happens to
+// have a recursive definition. The first two values in the sequence are
+// 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
+// previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
+// and so on. Define a recursive fibonacci(n) method that returns the nth
+// fibonacci number, with n=0 representing the start of the sequence.
+
+function fibonacci (n: number, nth: number) {
+  if (nth === 1) {
+    return 0;
+  } else if (nth === 2 || nth === 3) {
+    return 1;
+    }
+    else {
+    return n + fibonacci(n, nth-1) + fibonacci(n, nth-2);
+  }
+}
+
+console.log (fibonacci (0, 9));
