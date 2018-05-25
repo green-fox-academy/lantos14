9b1dbee4c758846e1459ae1254e1fb0e7ece23e0
lantos14 <lantos891213@gmail.com>
2018-05-25 14:00:12 +0200

    added animation with setTimeout

--- week-03/day-4/triangle-fractal/triangles-class.ts
+++ week-03/day-4/triangle-fractal/triangles-class.ts
@@ -32,13 +32,16 @@ class triangleSwarm {
     ctx.strokeStyle = 'black';
     ctx.fillStyle = `hsl(${Math.floor(Math.random() * 360) - count}, ${100 - count * 3}%, 50%)`
     ctx.fill();
-      this.draw (startX + size / 3, startY, size / 2, count -1);
-      this.draw (startX - size / 3, startY, size / 2, count -1);
-      this.draw (startX, startY + size / 2, size / 2, count -1);
+    let self: triangleSwarm = this;
+    setTimeout (function () {
+      self.draw (startX + size / 3, startY, size / 2, count -1);
+      self.draw (startX - size / 3, startY, size / 2, count -1);
+      self.draw (startX, startY + size / 2, size / 2, count -1);
+    }, 500)
   }
 }
-let swarm1 = new triangleSwarm (400, 50, 600, 10);
-swarm1.draw(400, 50, 600, 10);
+let swarm1 = new triangleSwarm (400, 50, 600, 8);
+swarm1.draw(400, 50, 600, 8);
 
 
 
