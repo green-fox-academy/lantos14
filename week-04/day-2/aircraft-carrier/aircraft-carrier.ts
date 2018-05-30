60e11133b6327c3aaabbd9542ecc735cceb80ca0
lantos14 <lantos891213@gmail.com>
2018-05-30 16:56:15 +0200

    did som fixes regarding the battle simulator, still not perfect though

--- week-04/day-2/aircraft-carrier/aircraft-carrier.ts
+++ week-04/day-2/aircraft-carrier/aircraft-carrier.ts
@@ -18,25 +18,32 @@ class AircraftCarrier {
     this.aircrafts.push(aircraft);
   }
 
-  fill() { // not including the priority method and has a problem when the carrier ammo storage runs out -- if I'll have time, I'll fix it
+  fill() { // at the last fill, before carrier ammo store runs out, function not works properly
     this.aircrafts.forEach(aircraft => {
       if (this.carrierAmmoStore <= 0) {
         aircraft.ammo = 0;
         return 'out of Ammo';
       }
       let ammoNeedForAircraft: number = aircraft.maxAmmo - aircraft.ammo;
+      if (ammoNeedForAircraft > this.carrierAmmoStore) {
+        aircraft.refill(this.carrierAmmoStore);
+        this.carrierAmmoStore = 0;
+      }
       aircraft.refill(aircraft.maxAmmo - aircraft.ammo);
       this.carrierAmmoStore -= ammoNeedForAircraft;
-      
+
     })
   }
 
   fight(carrier: AircraftCarrier): void {
     let totalDestruction: number = 0;
-    this.aircrafts.forEach(aircraft => {
-      totalDestruction += (aircraft.ammo * aircraft.baseDmg)
-    })
-    carrier.health -= totalDestruction;
+    if (this.carrierAmmoStore <= 0 && this.aircrafts[0].ammo === 0) { } else {
+      this.aircrafts.forEach(aircraft => {
+        totalDestruction += (aircraft.ammo * aircraft.baseDmg)
+        aircraft.ammo = 0;
+      })
+      carrier.health -= totalDestruction;
+    }
   }
 
   getStatus(): string {
@@ -55,47 +62,52 @@ class AircraftCarrier {
   }
 }
 
-let bismarck: AircraftCarrier = new AircraftCarrier('Bismarck', 20, 150000);
-let magellan: AircraftCarrier = new AircraftCarrier('Magellan', 30, 100000);
+let bismarck: AircraftCarrier = new AircraftCarrier('Bismarck', 105, 10000);
+let magellan: AircraftCarrier = new AircraftCarrier('Magellan', 100, 4000);
 
 //carrier-01 preparing to fight
 bismarck.add(new F16);
 bismarck.add(new F35);
-bismarck.fill();
+
 
 //carrier-02 preparing to fight
 magellan.add(new F35);
 magellan.add(new F35);
-magellan.fill();
+
 
 //fight
 let roundCount: number = 1;
 
-function navalWar (carrier1: AircraftCarrier, carrier2: AircraftCarrier) {
-  
-  console.log('Ship BISMARCK\n' + carrier1.getStatus());
-  console.log('Ship MAGELLAN\n' + carrier2.getStatus());
-  console.log(`--------ROUND ${roundCount}-----------`);
+function navalWar(carrier1: AircraftCarrier, carrier2: AircraftCarrier) {
+  carrier1.fill();
+  carrier2.fill();
+  console.log(`Ship ${carrier1.name}\n ${carrier1.getStatus()}`);
+  console.log(`Ship ${carrier2.name}\n ${carrier2.getStatus()}`);
+  console.log(`************ROUND ${roundCount}************`);
   carrier1.fight(carrier2);
-  console.log('--------BISMARCK ATTACKS-----------');
-  console.log('Ship BISMARCK\n' + carrier1.getStatus());
-  console.log('Ship MAGELLAN\n' + carrier2.getStatus());
+  console.log(`--------${carrier1.name} ATTACKS-----------`);
+  console.log(`Ship ${carrier1.name}\n ${carrier1.getStatus()}`);
+  console.log(`Ship ${carrier2.name}\n ${carrier2.getStatus()}`);
   console.log('-------------------');
-  if (carrier2.health < 0) {
+  if (carrier2.health <= 0) {
     console.log(`${carrier2.name} sunk to the bottom of the sea`)
     return;
   }
   carrier2.fight(carrier1);
-  console.log('--------MAGELLAN ATTACKS-----------');
-  console.log('Ship BISMARCK\n' + carrier1.getStatus());
-  console.log('Ship MAGELLAN\n' + carrier2.getStatus());
+  console.log(`--------${carrier2.name} ATTACKS-----------`);
+  console.log(`Ship ${carrier1.name}\n ${carrier1.getStatus()}`);
+  console.log(`Ship ${carrier2.name}\n ${carrier2.getStatus()}`);
   console.log('-------------------');
-  if (carrier1.health < 0) {
+  if (carrier1.health <= 0) {
     console.log(`${carrier1.name} sunk to the bottom of the sea`)
     return;
-  } 
+  }
+  if (carrier1.carrierAmmoStore <= 0 && carrier2.carrierAmmoStore <= 0) {
+    console.log('Both ships ammo store is dry. The naval battle ends with a tie')
+    return;
+  }
   roundCount += 1;
-  navalWar(carrier1,carrier2);
+  navalWar(carrier1, carrier2);
 }
 
-navalWar (bismarck, magellan);
+navalWar(bismarck, magellan);
