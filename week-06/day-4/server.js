06a2f83fbfa60ad1b9d0a06c6117eefee0860cd3
lantos14 <lantos891213@gmail.com>
2018-06-21 11:22:59 +0200

    added static files

--- week-06/day-4/server.js
+++ week-06/day-4/server.js
@@ -1,11 +1,29 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+//        root name       function  folder  <= static meanings
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!');
+  res.sendFile(path.join(__dirname, './index.html'))
+});
+
+app.get('/hola', (req, res) => {
+  res.send('Hola!!!!');
+});
+
+app.get('/greet/:message', (req, res) => {
+  // html input: http://localhost:3000/greet/hola?name=Fox
+  console.log(req.query);
+  // default parameter
+  let name = 'Fox';
+  if (req.query.name) {
+    name = req.query.name;
+  }
+  res.send(`${req.params.message}, ${name}`);
 });
 
 app.listen(PORT, () => {
