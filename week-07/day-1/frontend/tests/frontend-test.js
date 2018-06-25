f0ebc7fac959ffc074dfeb78b12cc2ca0766c233
lantos14 <lantos891213@gmail.com>
2018-06-25 15:49:33 +0200

    added some test scenarios

--- week-07/day-1/frontend/tests/frontend-test.js
+++ week-07/day-1/frontend/tests/frontend-test.js
@@ -4,22 +4,60 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('/', (t) => {
+// greeter
+test('greeter base', (t) => {
   request(app)
-  .get('/')
-  .expect('Content-Type', /text\/html/)
+    .get('/greeter')
+    .expect('Content-Type', /json/)
+    .expect(200, { error: "Please provide a name!" })
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
+
+test('greeter only name', (t) => {
+  request(app)
+    .get('/greeter?name=Tibi')
+    .expect('Content-Type', /json/)
+    .expect(200, { error: "Please provide a title!" })
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
+
+test('greeter name & title', (t) => {
+  request(app)
+    .get('/greeter?name=Tibi&title=student')
+    .expect('Content-Type', /json/)
+    .expect(200, { welcome_message: `Oh, hi there Tibi, my dear student!` })
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
+
+test('appenda base', (t) => {
+  request(app)
+  .get('/appenda/kuty')
+  .expect('Content-Type', /json/)
+  .expect(200, { appended: 'kutya'})
   .end((err, res) => {
+    t.error(err);
     t.end();
   });
 });
 
-test('/doubling', (t) => {
-  let input = 5; 
+test('appenda no appendable provided', (t) => {
   request(app)
-  .get('/doubling')
-  .expect({
-    received: 5,
-    result: 10
-  })
-  .end();
-})
+  .get('/appenda')
+  .expect('Content-Type', /json/)
+  .expect(404, { message: '404'})
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  });
+});
+
+// test post
