cbae3bba4e22113bd3dbc2327b60bc8f6a096cd9
lantos14 <lantos891213@gmail.com>
2018-06-26 22:27:33 +0200

    film listing added

--- week-07/day-2/ajax/star-wars/main.js
+++ week-07/day-2/ajax/star-wars/main.js
@@ -6,14 +6,16 @@ const http = new XMLHttpRequest();
 const input = document.querySelector("#search-term");
 const p = document.querySelector('#test');
 const leftList = document.querySelector('#left');
+const rightList = document.querySelector('#right');
 
 const button = document.querySelector('button');
+
 
 button.addEventListener('click', () => {
   while (leftList.firstChild) {
     leftList.removeChild(leftList.firstChild);
   }
-  
+
   http.open('GET', "https://swapi.co/api/people/", true);
   http.onload = () => {
     const response = JSON.parse(http.responseText);
@@ -21,15 +23,9 @@ button.addEventListener('click', () => {
       if (e.name.includes(input.value)) {
         let newLi = document.createElement('li');
         newLi.innerText = e.name;
-        newLi.setAttribute('class',`${e.height}`)
-        newLi.addEventListener('click', () => {
-          http.open('GET', 'https://swapi.co/api/vehicles/', true);
-          http.onload = () => {
-            const subresponse = JSON.parse(http.responseText);
-            console.log('subGET');
-            console.log(subresponse);
-          }
-        });
+
+        newLi.addEventListener('click', () => subFunc(e.films));
+
         leftList.appendChild(newLi);
       }
     });
@@ -38,3 +34,21 @@ button.addEventListener('click', () => {
   http.send();
 });
 
+let subFunc = (list) => {
+  for (let i = 0; i < list.length; i++) {
+    while (rightList.firstChild) {
+      rightList.removeChild(rightList.firstChild);
+    }
+    const http = new XMLHttpRequest;
+    http.open('GET', list[i], true)
+    http.onload = () => {
+      console.log(http.responseText);
+
+      const subResponse = JSON.parse(http.responseText);
+      let newFilm = document.createElement('li');
+      newFilm.innerText = subResponse.title;
+      rightList.appendChild(newFilm);
+    }
+    http.send();
+  }
+}
