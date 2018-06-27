8ddbee64fb81ce874040861273d6cb75faec9ca3
lantos14 <lantos891213@gmail.com>
2018-06-27 14:48:32 +0200

    added full-list endpoint, added some style

--- week-07/day-3/bookstore-endpoints/server.js
+++ week-07/day-3/bookstore-endpoints/server.js
@@ -36,6 +36,22 @@ app.get('/api/titles', (req, res) => {
   });
 });
 
+app.get('/api/full-list', (req, res) => {
+  let sql = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;';
+
+  conn.query(sql, (err, rows) => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    res.json({
+      fullList: rows,
+    });
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- week-07/day-3/bookstore-endpoints/static/main.js
+++ week-07/day-3/bookstore-endpoints/static/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const http = new XMLHttpRequest();
-const button = document.querySelector('button');
-const ul = document.querySelector('#list');
+const buttonTitle = document.querySelector('#titles');
+const buttonFullList = document.querySelector('#full-list-btn');
+const ul1 = document.querySelector('#list1');
+const fullListBody = document.querySelector('#full-list-tbody');
 
-button.addEventListener('click', () => {
+buttonTitle.addEventListener('click', () => {
 
   http.open('GET', "http://localhost:3000/api/titles", true);
   http.onload = () => {
@@ -12,8 +14,40 @@ button.addEventListener('click', () => {
     response.bookNames.forEach(title => {
       let newLi = document.createElement('li');
       newLi.innerText = title.book_name;
-      ul.appendChild(newLi);
+      ul1.appendChild(newLi);
     })
+  }
+  http.send();
+});
+
+buttonFullList.addEventListener('click', () => {
+
+  http.open('GET', "http://localhost:3000/api/full-list", true);
+  http.onload = () => {
+    let response = JSON.parse(http.response);
+
+    response.fullList.forEach(book => {
+      let newTRow = document.createElement('tr');
+      let tdName = document.createElement('td');
+      let tdAut = document.createElement('td');
+      let tdCat = document.createElement('td');
+      let tdPub = document.createElement('td');
+      let tdCost = document.createElement('td');
+
+      tdName.innerText = book.book_name;
+      tdAut.innerText = book.aut_name;
+      tdCat.innerText = book.cate_descrip;
+      tdPub.innerText = book.pub_name;
+      tdCost.innerText = book.book_price;
+
+      newTRow.appendChild(tdName);
+      newTRow.appendChild(tdAut);
+      newTRow.appendChild(tdCat);
+      newTRow.appendChild(tdPub);
+      newTRow.appendChild(tdCost);
+
+      fullListBody.appendChild(newTRow);
+    });
   }
   http.send();
 });
