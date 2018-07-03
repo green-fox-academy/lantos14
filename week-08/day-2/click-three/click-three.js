064d20f88608d6c72f7c52736c96399f688be801
lantos14 <lantos891213@gmail.com>
2018-07-03 13:53:20 +0200

    fixed event, when user clicks too soon

--- week-08/day-2/click-three/click-three.js
+++ week-08/day-2/click-three/click-three.js
@@ -9,13 +9,17 @@ let clickCount = 0;
 window.onload = () => {
 
   setTimeout(() => {
+    console.log("time is elapsed")
     timeIsElapsed = true;
   }, 5000)
 };
 
 btn.addEventListener('click', () => {
   clickCount++;
-  if (timeIsElapsed && clickCount === 3) {
+  if (!timeIsElapsed) {
+    alert("you clicked too soon, refresh the page");
+    clickCount += 3;
+  } else if (timeIsElapsed && clickCount === 3) {
     text.innerText = '5 seconds elapsed and clicked 3 times';
   }
 });
