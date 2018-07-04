82f08fcd9027c5a24af2a9d365ded745ce68c534
lantos14 <lantos891213@gmail.com>
2018-07-04 14:03:57 +0200

    added basic layout for movie selector

---
+++ week-08/day-3/03-movie-selector/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.querySelector('#genre');
+document.querySelector('#genre');
+
+document.querySelector('#movies');
+document.querySelectorAll('.scifi');
+document.querySelectorAll('.drama');
+document.querySelectorAll('.comedy');
+
+console.log
