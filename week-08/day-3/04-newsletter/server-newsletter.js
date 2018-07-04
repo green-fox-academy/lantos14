2e2feaa9e1cd6f4bae7f7cbd45d1a3ac70b4ebfe
lantos14 <lantos891213@gmail.com>
2018-07-04 16:56:39 +0200

    backend side is ready

--- week-08/day-3/04-newsletter/server-newsletter.js
+++ week-08/day-3/04-newsletter/server-newsletter.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'newsletter.html'));
+});
+
+app.post('/signup', (req, res) => {
+  let result = `<h1>Thanks ${req.body.username}, we will send updates to ${req.body.email}</h1>`;
+  res.send(result);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
