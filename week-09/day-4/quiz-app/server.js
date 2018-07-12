2762cc87c7712425bd3e8a51a693c37140f0914b
lantos14 <lantos891213@gmail.com>
2018-07-12 11:06:36 +0200

    set up server and /game endpoint

---
+++ week-09/day-4/quiz-app/server.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static('static'));
+// app.use('/static', express.static('static'));
+app.set('view engine', 'ejs');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'quiz',
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/game', (req, res) => {
+  let randomNum = Math.floor(Math.random() * 11);
+  let sql = `SELECT  answers.question_id AS questionId, question, answers.id, answers.answer, answers.is_correct AS correct FROM questions INNER JOIN answers ON questions.id = answers.question_id WHERE questions.id = ${randomNum};`;
+
+  conn.query(sql, (err, rows) => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    // preparing the response
+    let answerList = [];
+    parseAnswers(rows, answerList);
+
+    let sortedRows = {
+      id: rows[0].questionId,
+      question: rows[0].question,
+      answers: answerList,
+    };
+    // send
+    res.json(
+      sortedRows,
+    );
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const parseAnswers = (rows, resultList) => {
+  rows.forEach(row => {
+    resultList.push({
+      questionId: row.questionId,
+      answerId: row.id,
+      answer: row.answer,
+      is_correct: row.correct
+    })
+  });
+}
