e0e5d0e498e83b8c6389bd948d8b7a43f72629ff
lantos14 <lantos891213@gmail.com>
2018-07-12 16:44:04 +0200

    delete front end added

--- week-09/day-4/quiz-app/static/edit.js
+++ week-09/day-4/quiz-app/static/edit.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// grabbing elements
+const btnEdit = document.querySelector('#send-quest');
+const inputValues = document.querySelectorAll('input[type="text"]');
+const radioValues = document.querySelectorAll('input[type="radio"]');
+const deleteDiv = document.querySelector('#delete-div');
+
+// sub functions
 const getFormValues = (inputs) => {
   let result = [];
 
@@ -22,27 +29,58 @@ const getRadioValues = (inputs) => {
   return result;
 }
 
-const btnEdit = document.querySelector('#send-quest');
-const inputValues = document.querySelectorAll('input[type="text"]');
-const radioValues = document.querySelectorAll('input[type="radio"]');
+const fillDelList = () => {
+  const httpQList = new XMLHttpRequest();
+  httpQList.open('GET', 'http://localhost:3000/questions', true);
+  httpQList.onload = () => {
+    let questions = JSON.parse(httpQList.response);
+    questions.forEach(element => {
+      let divQ = document.createElement('div');
+      divQ.setAttribute('class', 'del-question');
+      let par = document.createElement('p');
+      par.innerText = element.question;
+      let btn = document.createElement('button');
+      btn.innerText = 'Delete';
+      btn.dataset.idq = element.id;
+
+      divQ.appendChild(par);
+      divQ.appendChild(btn);
+      deleteDiv.appendChild(divQ);
+    });
+  }
+  httpQList.send();
+}
+
+// delete req
+const delReq = (id) => {
+  const httpDelList = new XMLHttpRequest();
+  httpDelList.open('DELETE', `http://localhost:3000/posts/${id}`);
+  httpDelList.send();
+}
+
+// filling up delete list 
+fillDelList();
 
 btnEdit.addEventListener('click', (e) => {
   e.preventDefault();
   const httpPost = new XMLHttpRequest();
   httpPost.open('POST', 'http://localhost:3000/questions', true);
   httpPost.setRequestHeader('Content-type', 'application/json');
-  let postData =  getFormValues(inputValues);
-  let checkRadioList =  getRadioValues(radioValues);
+  let postData = getFormValues(inputValues);
+  let checkRadioList = getRadioValues(radioValues);
   console.log('postData: ', postData);
   console.log('checkRadioList: ', checkRadioList);
   httpPost.send(JSON.stringify({
     question: postData[0],
-    a1: {text: postData[1], correct: checkRadioList[0]},
-    a2: {text: postData[2], correct: checkRadioList[1]},
-    a3: {text: postData[3], correct: checkRadioList[2]},
-    a4: {text: postData[4], correct: checkRadioList[3]},
+    a1: { text: postData[1], correct: checkRadioList[0] },
+    a2: { text: postData[2], correct: checkRadioList[1] },
+    a3: { text: postData[3], correct: checkRadioList[2] },
+    a4: { text: postData[4], correct: checkRadioList[3] },
   }));
 });
 
-// sub functions
-
+deleteDiv.addEventListener('click', (e) => {
+  if (e.target.dataset.idq) {
+    delReq(e.target.dataset.idq);
+  }
+});
